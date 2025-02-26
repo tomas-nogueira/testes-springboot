51d6bee92aa9a4650563576e4d0337bbbca29aaa
tomas-nogueira <tomasmnogueira2006@gmail.com>
2025-02-26 09:31:32 -0300

    Aprendendo os conceitos de bean e components e quando utilizar

--- src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -1,13 +1,26 @@
 package dio.springboot;
 
+import com.google.gson.Gson;
+import dio.springboot.ConversorJson;
+import dio.springboot.ViaCepResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça da Sé\",\"localidade\": \"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
 	}
 
 }
